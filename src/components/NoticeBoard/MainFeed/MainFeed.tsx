44e540905605f22e0371d00a3fe35fb6a2d7c327
Saurab Salhotra <saurabsalhotra@gmail.com>
2021-03-30 05:45:45 +0530

    Fixed some iterable error

--- src/components/NoticeBoard/MainFeed/MainFeed.tsx
+++ src/components/NoticeBoard/MainFeed/MainFeed.tsx
@@ -1,23 +1,24 @@
 import { IPost } from "../../../../types/postTypes";
 import Post from "./Post";
 
-function MainFeed({posts}:{posts:IPost[]}) {
+function MainFeed({ posts }: { posts: IPost[] }) {
+  let postsToRender = [];
 
-    let postsToRender = [];
-
-    for(let post of posts){
-        postsToRender.push(<Post post={post}/>)
+  if (posts instanceof Array && posts.length > 0) {
+    for (let post of posts) {
+      postsToRender.push(<Post post={post} />);
     }
+  }
 
-    return (
-        <>
-            <div className="conatiner mt-3 px-3">
-                <h5>Notice Board</h5>
-                <hr className="my-0"></hr>
-                {postsToRender}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="conatiner mt-3 px-3">
+        <h5>Notice Board</h5>
+        <hr className="my-0"></hr>
+        {postsToRender}
+      </div>
+    </>
+  );
 }
 
 export default MainFeed;
